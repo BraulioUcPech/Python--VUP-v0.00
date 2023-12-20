25a8645bda82f747643c1477d9a6b005b98a082b
Bra-Ülio <braulioalexson@gmail.com>
2023-12-19 18:44:20 -0600

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = "sk-1VowbNHFv..."
+openai.api_key =
 completion = openai.Completion.create(engine="gpt-3.5-turbo",
                          prompt="¿Que es ChatGPT?.",
                          mat_totokens = "2048")
